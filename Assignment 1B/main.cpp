6732e8e3a134d99ba4e0497304d4cb0208af955c
[Iris Bai] <[valid-email]>
2020-04-22 16:54:49 -0700

    Initial Commit

---
+++ Assignment 1B/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 1B
+//
+//  Created by Iris Bai on 4/22/20.
+//  Copyright © 2020 Iris Bai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
