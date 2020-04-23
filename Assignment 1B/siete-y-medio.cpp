f3f70619f87c267cd1a5c1db041d9cac23d73164
[Iris Bai] <[valid-email]>
2020-04-22 17:00:50 -0700

    template files for siete y medio

---
+++ Assignment 1B/siete-y-medio.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Assignment 1B
+//
+//  Created by Iris Bai on 4/22/20.
+//  Copyright © 2020 Iris Bai. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+   /* --STATEMENTS-- */
+   return 0;
+}
